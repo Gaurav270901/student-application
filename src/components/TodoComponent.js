7ca3f9089f25c42e778c607562ba897986c73097
Gaurav270901 <shimpigaurav27s@gmail.com>
2024-01-31 12:46:53 +0530

    updated counter

--- src/components/TodoComponent.js
+++ src/components/TodoComponent.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import './InputCounter.css';
-
+import "./InputCounter.css" ; 
 const InputCounter = () => {
   const [entries, setEntries] = useState([]);
   const [counter, setCounter] = useState(0);
+  const [deleteIndex, setDeleteIndex] = useState('');
+  const [updateIndex, setUpdateIndex] = useState('');
+  const [updatedValue, setUpdatedValue] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -13,19 +15,74 @@ const InputCounter = () => {
     event.target.elements.entry.value = ''; // Clear input field
   };
 
+  const handleDelete = () => {
+    setEntries((prevEntries) => {
+      const newEntries = [...prevEntries];
+      newEntries.splice(parseInt(deleteIndex, 10), 1);
+      return newEntries;
+    });
+    setDeleteIndex('');
+  };
+
+  const handleUpdate = () => {
+    setEntries((prevEntries) => {
+      const newEntries = [...prevEntries];
+      newEntries[parseInt(updateIndex, 10)] = updatedValue;
+      return newEntries;
+    });
+    setUpdateIndex('');
+    setUpdatedValue('');
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="entry">Enter text:</label>
-      <input type="text" id="entry" />
-      <button type="submit">Submit</button>
-      <p>Entries:</p>
-      <ul>
-        {entries.map((entry, index) => (
-          <li key={index}>{entry}</li>
-        ))}
-      </ul>
+    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+      <div>
       <p>Counter: {counter}</p>
-    </form>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="entry">Enter text:</label>
+          <input type="text" id="entry" />
+          <button type="submit">Submit</button>
+        </form>
+
+        <p>Entries:</p>
+        <ul>
+          {entries.map((entry, index) => (
+            <li key={index}>{entry}</li>
+          ))}
+        </ul>
+        
+      </div>
+
+      <div>
+        <p>Delete element at index:</p>
+        <input
+          type="number"
+          value={deleteIndex}
+          onChange={(e) => setDeleteIndex(e.target.value)}
+        />
+        <button type="button" onClick={handleDelete}>
+          Delete
+        </button>
+
+        <p>Update element at index:</p>
+
+        <input
+          type="number"
+          placeholder='Enter the index to be changed'
+          value={updateIndex}
+          onChange={(e) => setUpdateIndex(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder='Enter the new value'
+          value={updatedValue}
+          onChange={(e) => setUpdatedValue(e.target.value)}
+        />
+        <button type="button" onClick={handleUpdate}>
+          Update
+        </button>
+      </div>
+    </div>
   );
 };
 
